75e1d618f83ad0a9ad37a6844fb047feda8924c9
nina <juan.amado.sicat.cleto@gmail.com>
2025-07-26 01:34:46 +0800

    Third Commit

--- src/FileMakerCommon.java
+++ src/FileMakerCommon.java
@@ -1,2 +1,55 @@
+import java.util.Date;
+
 public class FileMakerCommon {
+
+    private String word;
+    private String sentence;
+    private String book;
+    private Date date;
+
+    public FileMakerCommon()
+    {
+        System.out.println("First Constructor Called");
+    }
+
+    public FileMakerCommon(String word, String sentence, String book, Date date)
+    {
+        System.out.println("Second Constructor Called");
+        this.word = word;
+        this.sentence = sentence;
+        this.book = book;
+        this.date = date;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void setSentence(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getSentence() {
+        return sentence;
+    }
+
+    public String getBook() {
+        return book;
+    }
+
+    public Date getDate() {
+        return date;
+    }
 }
